c05db4e2b1a4588df1038cbaf64b05cc93244973
StefanUnterholzner <02untste@rgtfo-me.it>
2020-05-02 17:25:49 +0200

    Konstruktor prgrammiert. Eine zufällige Farbe und eine zufällige, frei Possition wird in Konstruktor gewählt.

--- Collision/Collision/Program.cs
+++ Collision/Collision/Program.cs
@@ -20,7 +20,8 @@ namespace ConsoleApplication1
         static int[,] feld = new int[seite, seite];
 
         class einer
-        {
+        {   
+            
             // Private Eigenschaften
 
             // Öffentliche Eigenschaften
@@ -29,6 +30,70 @@ namespace ConsoleApplication1
             // Konstruktor
             public einer()
             {
+                Random RG = new Random();
+                //wählt eine zufällige Farebe aus
+                #region Farbe
+                switch (RG.Next(0, 15))
+                {
+                    case 0:
+                        farbe = ConsoleColor.Black;
+                        break;
+                    case 1:
+                        farbe = ConsoleColor.Blue;
+                        break;
+                    case 2:
+                        farbe = ConsoleColor.Cyan;
+                        break;
+                    case 3:
+                        farbe = ConsoleColor.DarkBlue;
+                        break;
+                    case 4:
+                        farbe = ConsoleColor.DarkCyan;
+                        break;
+                    case 5:
+                        farbe = ConsoleColor.DarkGray;
+                        break;
+                    case 6:
+                        farbe = ConsoleColor.DarkGreen;
+                        break;
+                    case 7:
+                        farbe = ConsoleColor.DarkMagenta;
+                        break;
+                    case 8:
+                        farbe = ConsoleColor.DarkRed;
+                        break;
+                    case 9:
+                        farbe = ConsoleColor.DarkYellow;
+                        break;
+                    case 10:
+                        farbe = ConsoleColor.Gray;
+                        break;
+                    case 11:
+                        farbe = ConsoleColor.Green;
+                        break;
+                    case 12:
+                        farbe = ConsoleColor.Magenta;
+                        break;
+                    case 13:
+                        farbe = ConsoleColor.Red;
+                        break;
+                    case 14:
+                        farbe = ConsoleColor.White;
+                        break;
+                    case 15:
+                        farbe = ConsoleColor.Yellow;
+                        break;
+                }
+                #endregion
+                //Wählt eine zufällige, freie Possition aus
+                // Achtung, wenn mehr als 2500 Einer erzeugt werden entsteht eine Endlosschleife
+                do
+                {
+                    posx = RG.Next(0, seite);
+                    posy = RG.Next(0, seite);
+                } while (feld[posx, posy] == 1);
+                feld[posx, posy] = 1;
+
             }
             //Private Methoden
             void show()
